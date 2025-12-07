a93a12f0e2a3d208a677582f3ff1bd07659a6427
Yousef0sama <yousefosama1885@gmail.com>
2025-12-07 18:09:08 +0200

    fix images path

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 
 export default function Intro() {
   return (
-    <section id="intro" className="bg-[url('/images/bg-tablet-pattern.svg')] bg-no-repeat bg-[right_-11rem_top_-9rem] min-h-screen md:bg-[length:700px] bg-[length:500px]">
+    <section id="intro" className="bg-[url('images/bg-tablet-pattern.svg')] bg-no-repeat bg-[right_-11rem_top_-9rem] min-h-screen md:bg-[length:700px] bg-[length:500px]">
       <Header />
       {/* Hero */}
       <div className="flex flex-wrap-reverse md:flex-nowrap md:justify-between justify-center md:items-center px-16 lg:px-32 xl:px-48 pb-4 min-h-[calc(100vh-7rem)] text-center md:text-left gap-y-12">
@@ -24,7 +24,7 @@ export default function Intro() {
         </div>
         <div className="flex md:justify-end w-xs lg:w-sm xl:w-md min-w-xs">
           <Image
-            src="/images/illustration-intro.svg"
+            src="images/illustration-intro.svg"
             alt="Intro Illustration"
             width={500}
             height={500}
